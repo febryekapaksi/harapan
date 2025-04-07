d604180c9237307c8882e9807d6af2e81c963f9b
febryekapaksi <febry.eka2012@gmail.com>
2025-04-07 10:01:43 +0700

    update product master, add product retail di form

--- application/modules/product_master/views/add.php
+++ application/modules/product_master/views/add.php
@@ -1,104 +1,111 @@
 <?php
-    $ENABLE_ADD     = has_permission('Product_Master.Add');
-    $ENABLE_MANAGE  = has_permission('Product_Master.Manage');
-    $ENABLE_VIEW    = has_permission('Product_Master.View');
-    $ENABLE_DELETE  = has_permission('Product_Master.Delete');
+$ENABLE_ADD     = has_permission('Product_Master.Add');
+$ENABLE_MANAGE  = has_permission('Product_Master.Manage');
+$ENABLE_VIEW    = has_permission('Product_Master.View');
+$ENABLE_DELETE  = has_permission('Product_Master.Delete');
 
-	$id = (!empty($listData[0]->id))?$listData[0]->id:'';
-	$code_lv1 = (!empty($listData[0]->code_lv1))?$listData[0]->code_lv1:'';
-	$code_lv2 = (!empty($listData[0]->code_lv2))?$listData[0]->code_lv2:'';
-	$code_lv3 = (!empty($listData[0]->code_lv3))?$listData[0]->code_lv3:'';
-	$code_lv4 = (!empty($listData[0]->code_lv4))?$listData[0]->code_lv4:'';
-	$nama = (!empty($listData[0]->nama))?$listData[0]->nama:'';
+$id = (!empty($listData[0]->id)) ? $listData[0]->id : '';
+$code_lv1 = (!empty($listData[0]->code_lv1)) ? $listData[0]->code_lv1 : '';
+$code_lv2 = (!empty($listData[0]->code_lv2)) ? $listData[0]->code_lv2 : '';
+$code_lv3 = (!empty($listData[0]->code_lv3)) ? $listData[0]->code_lv3 : '';
+$code_lv4 = (!empty($listData[0]->code_lv4)) ? $listData[0]->code_lv4 : '';
+$nama = (!empty($listData[0]->nama)) ? $listData[0]->nama : '';
+$nama = (!empty($listData[0]->retail)) ? $listData[0]->retail : '';
 
-	$code = (!empty($listData[0]->code))?$listData[0]->code:'';
-	$trade_name = (!empty($listData[0]->trade_name))?$listData[0]->trade_name:'';
-	$max_stok = (!empty($listData[0]->max_stok))?$listData[0]->max_stok:'';
-	$min_stok = (!empty($listData[0]->min_stok))?$listData[0]->min_stok:'';
+$code = (!empty($listData[0]->code)) ? $listData[0]->code : '';
+$trade_name = (!empty($listData[0]->trade_name)) ? $listData[0]->trade_name : '';
+$max_stok = (!empty($listData[0]->max_stok)) ? $listData[0]->max_stok : '';
+$min_stok = (!empty($listData[0]->min_stok)) ? $listData[0]->min_stok : '';
 
-	$id_unit_packing = (!empty($listData[0]->id_unit_packing))?$listData[0]->id_unit_packing:'';
-	$konversi = (!empty($listData[0]->konversi))?$listData[0]->konversi:'';
-	$id_unit = (!empty($listData[0]->id_unit))?$listData[0]->id_unit:'';
+$id_unit_packing = (!empty($listData[0]->id_unit_packing)) ? $listData[0]->id_unit_packing : '';
+$konversi = (!empty($listData[0]->konversi)) ? $listData[0]->konversi : '';
+$id_unit = (!empty($listData[0]->id_unit)) ? $listData[0]->id_unit : '';
 
-	$length = (!empty($listData[0]->length))?$listData[0]->length:'';
-	$wide 	= (!empty($listData[0]->wide))?$listData[0]->wide:'';
-	$high 	= (!empty($listData[0]->high))?$listData[0]->high:'';
-	$cub 	= (!empty($listData[0]->cub))?$listData[0]->cub:'';
+$length = (!empty($listData[0]->length)) ? $listData[0]->length : '';
+$wide 	= (!empty($listData[0]->wide)) ? $listData[0]->wide : '';
+$high 	= (!empty($listData[0]->high)) ? $listData[0]->high : '';
+$cub 	= (!empty($listData[0]->cub)) ? $listData[0]->cub : '';
 
-	$file_msds 	= (!empty($listData[0]->file_msds))?$listData[0]->file_msds:'';
+$file_msds 	= (!empty($listData[0]->file_msds)) ? $listData[0]->file_msds : '';
 
-	$status1 = (!empty($listData[0]->status) AND $listData[0]->status == '1')?'checked':'';
-	$status2 = (!empty($listData[0]->status) AND $listData[0]->status == '2')?'checked':'';
+$status1 = (!empty($listData[0]->status) and $listData[0]->status == '1') ? 'checked' : '';
+$status2 = (!empty($listData[0]->status) and $listData[0]->status == '2') ? 'checked' : '';
 ?>
 <div class="box box-primary">
 	<div class="box-body">
-		<form id="data_form_master_product" method="post"  autocomplete="off" enctype='multiple/form-data'>
+		<form id="data_form_master_product" method="post" autocomplete="off" enctype='multiple/form-data'>
 			<div class="form-group row">
 				<div class="col-md-2">
-				<label for="">Product Type <span class='text-danger'>*</span></label>
+					<label for="">Product Type <span class='text-danger'>*</span></label>
 				</div>
 				<div class="col-md-10">
-				<select name="code_lv1" id="code_lv1" class='chosen-select'>
-					<option value="0">Select Product Type</option>
-					<?php
-					foreach ($listLevel1 as $key => $value) {
-						$selected = ($code_lv1 == $value['code_lv1'])?'selected':'';
-						echo "<option value='".$value['code_lv1']."' ".$selected.">".strtoupper($value['nama'])."</option>";
-					}
-					?>
-				</select>
-				</div>
-			</div>
-			<div class="form-group row">
-				<div class="col-md-2">
-				<label for="">Product Category <span class='text-danger'>*</span></label>
-				</div>
-				<div class="col-md-10">
-				<select name="code_lv2" id="code_lv2" class='chosen-select'>
-					<?php
-					if(!empty($id) AND !empty($listLevel2)){
-						echo "<option value='0'>Select Product Category</option>";
-						foreach ($listLevel2 as $key => $value) {
-							$selected = ($code_lv2 == $value['code_lv2'])?'selected':'';
-							echo "<option value='".$value['code_lv2']."' ".$selected.">".strtoupper($value['nama'])."</option>";
+					<select name="code_lv1" id="code_lv1" class='chosen-select'>
+						<option value="0">Select Product Type</option>
+						<?php
+						foreach ($listLevel1 as $key => $value) {
+							$selected = ($code_lv1 == $value['code_lv1']) ? 'selected' : '';
+							echo "<option value='" . $value['code_lv1'] . "' " . $selected . ">" . strtoupper($value['nama']) . "</option>";
 						}
-					}
-					else{
-						echo "<option value='0'>List Empty</option>";
-					}
-					?>
-				</select>
+						?>
+					</select>
 				</div>
 			</div>
 			<div class="form-group row">
 				<div class="col-md-2">
-				<label for="">Product Jenis <span class='text-danger'>*</span></label>
+					<label for="">Product Category <span class='text-danger'>*</span></label>
 				</div>
 				<div class="col-md-10">
-				<select name="code_lv3" id="code_lv3" class='chosen-select'>
-					<?php
-					if(!empty($id) AND !empty($listLevel3)){
-						echo "<option value='0'>Select Product Jenis</option>";
-						foreach ($listLevel3 as $key => $value) {
-							$selected = ($code_lv3 == $value['code_lv3'])?'selected':'';
-							echo "<option value='".$value['code_lv3']."' ".$selected.">".strtoupper($value['nama'])."</option>";
+					<select name="code_lv2" id="code_lv2" class='chosen-select'>
+						<?php
+						if (!empty($id) and !empty($listLevel2)) {
+							echo "<option value='0'>Select Product Category</option>";
+							foreach ($listLevel2 as $key => $value) {
+								$selected = ($code_lv2 == $value['code_lv2']) ? 'selected' : '';
+								echo "<option value='" . $value['code_lv2'] . "' " . $selected . ">" . strtoupper($value['nama']) . "</option>";
+							}
+						} else {
+							echo "<option value='0'>List Empty</option>";
 						}
-					}
-					else{
-						echo "<option value='0'>List Empty</option>";
-					}
-					?>
-				</select>
+						?>
+					</select>
 				</div>
 			</div>
 			<div class="form-group row">
 				<div class="col-md-2">
-				<label for="">Product Master <span class='text-danger'>*</span></label>
+					<label for="">Product Jenis <span class='text-danger'>*</span></label>
 				</div>
 				<div class="col-md-10">
-				<input type="hidden" class="form-control" id="id" name="id" value='<?=$id;?>'>
-				<input type="hidden" class="form-control" id="code_lv4" name="code_lv4" value='<?=$code_lv4;?>'>
-				<input type="text" class="form-control" id="nama" required name="nama" placeholder="Product Type" value='<?=$nama;?>'>
+					<select name="code_lv3" id="code_lv3" class='chosen-select'>
+						<?php
+						if (!empty($id) and !empty($listLevel3)) {
+							echo "<option value='0'>Select Product Jenis</option>";
+							foreach ($listLevel3 as $key => $value) {
+								$selected = ($code_lv3 == $value['code_lv3']) ? 'selected' : '';
+								echo "<option value='" . $value['code_lv3'] . "' " . $selected . ">" . strtoupper($value['nama']) . "</option>";
+							}
+						} else {
+							echo "<option value='0'>List Empty</option>";
+						}
+						?>
+					</select>
+				</div>
+			</div>
+			<div class="form-group row">
+				<div class="col-md-2">
+					<label for="">Product Master <span class='text-danger'>*</span></label>
+				</div>
+				<div class="col-md-10">
+					<input type="hidden" class="form-control" id="id" name="id" value='<?= $id; ?>'>
+					<input type="hidden" class="form-control" id="code_lv4" name="code_lv4" value='<?= $code_lv4; ?>'>
+					<input type="text" class="form-control" id="nama" required name="nama" placeholder="Product Type" value='<?= $nama; ?>'>
+				</div>
+			</div>
+			<div class="form-group row">
+				<div class="col-md-2">
+					<label for="">Product Retail</label>
+				</div>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="retail" name="retail" placeholder="Product Retail" value="<?= $retail ?>">
 				</div>
 			</div>
 			<hr>
@@ -107,14 +114,14 @@
 					<label>Product Code</label>
 				</div>
 				<div class="col-md-4">
-					<input type="text" class="form-control" id="code" name="code" value='<?=$code;?>' placeholder="Product Code">
+					<input type="text" class="form-control" id="code" name="code" value='<?= $code; ?>' placeholder="Product Code">
 					<span style='cursor:pointer;' class='text-primary' id='updateManualCode'>Update Code Program</span>
 				</div>
 				<div class="col-md-2">
 					<label>Trade Name</label>
 				</div>
 				<div class="col-md-4">
-					<input type="text" class="form-control" id="trade_name" name="trade_name" value='<?=$trade_name;?>' placeholder="Trade Name">
+					<input type="text" class="form-control" id="trade_name" name="trade_name" value='<?= $trade_name; ?>' placeholder="Trade Name">
 				</div>
 			</div>
 			<div class="form-group row">
@@ -124,15 +131,15 @@
 				<div class="col-md-2">
 					<select id="id_unit_packing" name="id_unit_packing" class="form-control input-md chosen-select">
 						<option value="0">Select An Option</option>
-						<?php foreach ($satuan_packing as $value){
-						$sel = ($value->id == $id_unit_packing)?'selected':'';
+						<?php foreach ($satuan_packing as $value) {
+							$sel = ($value->id == $id_unit_packing) ? 'selected' : '';
 						?>
-						<option value="<?= $value->id;?>" <?=$sel;?>><?= strtoupper(strtolower($value->code))?></option>
+							<option value="<?= $value->id; ?>" <?= $sel; ?>><?= strtoupper(strtolower($value->code)) ?></option>
 						<?php } ?>
 					</select>
 				</div>
 				<div class="col-md-2">
-					<input type="text" id="konversi" name="konversi" class="form-control input-md maskM" placeholder="Conversion" value='<?=$konversi;?>'>
+					<input type="text" id="konversi" name="konversi" class="form-control input-md maskM" placeholder="Conversion" value='<?= $konversi; ?>'>
 				</div>
 				<div class="col-md-2">
 					<label>Unit Measurement</label>
@@ -140,10 +147,10 @@
 				<div class="col-md-2">
 					<select id="id_unit" name="id_unit" class="form-control input-md chosen-select">
 						<option value="0">Select An Option</option>
-						<?php foreach ($satuan as $value){
-						$sel = ($value->id == $id_unit)?'selected':'';
+						<?php foreach ($satuan as $value) {
+							$sel = ($value->id == $id_unit) ? 'selected' : '';
 						?>
-						<option value="<?= $value->id;?>" <?=$sel;?>><?= strtoupper($value->code)?></option>
+							<option value="<?= $value->id; ?>" <?= $sel; ?>><?= strtoupper($value->code) ?></option>
 						<?php } ?>
 					</select>
 				</div>
@@ -153,13 +160,13 @@
 					<label>MOQ</label>
 				</div>
 				<div class="col-md-4">
-					<input type="text" class="form-control maskM" id="max_stok" name="max_stok" value='<?=$max_stok;?>' placeholder="MOQ">
+					<input type="text" class="form-control maskM" id="max_stok" name="max_stok" value='<?= $max_stok; ?>' placeholder="MOQ">
 				</div>
 				<div class="col-md-2">
 					<label>Minimum Stok</label>
 				</div>
 				<div class="col-md-4">
-					<input type="text" class="form-control maskM" id="min_stok" name="min_stok" value='<?=$min_stok;?>' placeholder="Minimum Stok">
+					<input type="text" class="form-control maskM" id="min_stok" name="min_stok" value='<?= $min_stok; ?>' placeholder="Minimum Stok">
 				</div>
 			</div>
 			<div class="form-group row">
@@ -168,10 +175,10 @@
 				</div>
 				<div class="col-md-10">
 					<div class="form-group">
-						<input type="file" name='photo' id="photo">	
+						<input type="file" name='photo' id="photo">
 					</div>
-					<?php if(!empty($file_msds)){ ?>
-						<a href='<?=base_url().$file_msds;?>' target='_blank' class="help-block" title='Download'>Download File</a>
+					<?php if (!empty($file_msds)) { ?>
+						<a href='<?= base_url() . $file_msds; ?>' target='_blank' class="help-block" title='Download'>Download File</a>
 					<?php } ?>
 				</div>
 			</div>
@@ -181,13 +188,13 @@
 					<label>Dimensi (L,W,H)</label>
 				</div>
 				<div class="col-md-3">
-					<input type="text" class="form-control maskM getCub" id="length" name="length" value='<?=$length;?>' placeholder="Length">
+					<input type="text" class="form-control maskM getCub" id="length" name="length" value='<?= $length; ?>' placeholder="Length">
 				</div>
 				<div class="col-md-3">
-					<input type="text" class="form-control maskM getCub" id="wide" name="wide" value='<?=$wide;?>' placeholder="Wide">
+					<input type="text" class="form-control maskM getCub" id="wide" name="wide" value='<?= $wide; ?>' placeholder="Wide">
 				</div>
 				<div class="col-md-3">
-					<input type="text" class="form-control maskM getCub" id="high" name="high" value='<?=$high;?>' placeholder="High">
+					<input type="text" class="form-control maskM getCub" id="high" name="high" value='<?= $high; ?>' placeholder="High">
 				</div>
 			</div>
 
@@ -196,29 +203,29 @@
 					<label>CBM</label>
 				</div>
 				<div class="col-md-3">
-					<input type="text" class="form-control" id="cub" name="cub" placeholder="CBM" readonly value='<?=$cub;?>'>
+					<input type="text" class="form-control" id="cub" name="cub" placeholder="CBM" readonly value='<?= $cub; ?>'>
 				</div>
 			</div>
-			<?php if(!empty($id)){ ?>
-			<div class="form-group row">
-				<div class="col-md-2">
-					<label for="">Status</label>
+			<?php if (!empty($id)) { ?>
+				<div class="form-group row">
+					<div class="col-md-2">
+						<label for="">Status</label>
+					</div>
+					<div class="col-md-4">
+						<label>
+							<input type="radio" class="radio-control" name="status" value="1" <?= $status1; ?>> Aktif
+						</label>
+						&nbsp &nbsp &nbsp
+						<label>
+							<input type="radio" class="radio-control" name="status" value="0" <?= $status2; ?>> Non-Aktif
+						</label>
+					</div>
 				</div>
-				<div class="col-md-4">
-					<label>
-					<input type="radio" class="radio-control" name="status" value="1" <?=$status1;?>> Aktif
-					</label>
-					&nbsp &nbsp &nbsp
-					<label>
-					<input type="radio" class="radio-control" name="status" value="0" <?=$status2;?>> Non-Aktif
-					</label>
-				</div>
-			</div>
 			<?php } ?>
 			<div class="form-group row">
 				<div class="col-md-2"></div>
 				<div class="col-md-10">
-				<button type="button" class="btn btn-primary" name="save" id="save"><i class="fa fa-save"></i> Save</button>
+					<button type="button" class="btn btn-primary" name="save" id="save"><i class="fa fa-save"></i> Save</button>
 				</div>
 			</div>
 		</form>
@@ -226,8 +233,10 @@
 </div>
 
 <script>
-	$(document).ready(function(){
-    	$('.chosen-select').select2({width: '100%'});
+	$(document).ready(function() {
+		$('.chosen-select').select2({
+			width: '100%'
+		});
 		$('.maskM').autoNumeric();
-  	});
+	});
 </script>
