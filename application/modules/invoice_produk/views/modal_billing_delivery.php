ec687b22abcb9f26271d33ea20f391be33422417
SamSyamsudin <syams.69@gmail.com>
2025-08-29 14:07:54 +0700

    update lagi

--- application/modules/invoice_produk/views/modal_billing_delivery.php
+++ application/modules/invoice_produk/views/modal_billing_delivery.php
@@ -277,13 +277,8 @@ $tipe_billing = $results['tipe_billing'];
             <td align="right"><input type="hidden" id="total" name="total" value="<?= $total_tagihan + $grand_total_beli ?>" class="form-control" readonly />
                 <input type="text" id="total31" name="total3" value="<?= $total_tagihan + $grand_total_beli  ?>" class="form-control" readonly />
             </td>
-<<<<<<< HEAD
             <td align="right"><input type="hidden" id="total2" name="total2" value="<?= $nilai_ppn+$excludeppn+$grand_total_beli ?>" class="form-control" readonly />
                 <input type="text" id="total41" name="total4" value="<?= $nilai_ppn+$excludeppn+$grand_total_beli ?>" class="form-control" readonly />
-=======
-            <td align="right"><input type="hidden" id="total2" name="total2" value="<?= $nilai_ppn + $dpp + $grand_total_beli ?>" class="form-control" readonly />
-                <input type="text" id="total41" name="total4" value="<?= $nilai_ppn + $dpp + $grand_total_beli ?>" class="form-control" readonly />
->>>>>>> d838864efd433c9f7b1e7a0562c5eb49f8099df9
             </td>
 
         </tr>
