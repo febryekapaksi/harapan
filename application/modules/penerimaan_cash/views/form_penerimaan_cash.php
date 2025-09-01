b22b12f3475e88fc8d917ed26f41020145c42f08
SamSyamsudin <syams.69@gmail.com>
2025-09-01 22:17:13 +0700

    update view form penerimaan cash

--- application/modules/penerimaan_cash/views/form_penerimaan_cash.php
+++ application/modules/penerimaan_cash/views/form_penerimaan_cash.php
@@ -94,6 +94,67 @@
                     </div>
                 </div>
             </div>
+
+            <table class="table table-bordered table-hover">
+    <thead>
+        <tr bgcolor='#9acfea'>
+            <th>
+                <center>Tanggal</center>
+            </th>
+            <th>
+                <center>Tipe</center>
+            </th>
+            <th>
+                <center>No. COA</center>
+            </th>
+            <th>
+                <center>Debit</center>
+            </th>
+            <th>
+                <center>Kredit</center>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr bgcolor='#DCDCDC'>
+            <td><input type="date" id="tgl_jurnal1" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
+            <td><input type="text" id="type1" name="type[]" value="JV" class="form-control" readonly /></td>
+            <td><input type="text" id="no_coa1" name="no_coa[]" value="1102-01-04" class="form-control" readonly /></td>
+            <td><input type="hidden" id="debet1" name="debet[]" value="0" class="form-control" readonly />
+                <input type="text" id="debet21" name="debet2[]" value="0" class="form-control" readonly />
+            </td>
+            <td><input type="hidden" id="kredit1" name="kredit[]" value="0" class="form-control" readonly />
+                <input type="text" id="kredit21" name="kredit2[]" value="0" class="form-control" readonly />
+            </td>
+
+        </tr>
+        <tr bgcolor='#DCDCDC'>
+            <td><input type="date" id="tgl_jurnal2" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
+            <td><input type="text" id="type2" name="type[]" value="JV" class="form-control" readonly /></td>
+            <td><input type="text" id="no_coa2" name="no_coa[]" value="1102-01-01" class="form-control" readonly /></td>
+            <td><input type="hidden" id="debet2" name="debet[]" value="0" class="form-control" readonly />
+                <input type="text" id="debet22" name="debet2[]" value="0" class="form-control" readonly />
+            </td>
+            <td><input type="hidden" id="kredit2" name="kredit[]" value="0" class="form-control" readonly />
+                <input type="text" id="kredit22" name="kredit2[]" value="0" class="form-control" readonly />
+            </td>
+
+        </tr>
+        
+
+         <tr bgcolor='#DCDCDC'>
+            <td colspan="3" align="right"><b>TOTAL</b></td>
+            <td align="right"><input type="hidden" id="total" name="total" value="0" class="form-control" readonly />
+                <input type="text" id="total31" name="total3" value="0" class="form-control" readonly />
+            </td>
+            <td align="right"><input type="hidden" id="total2" name="total2" value="0" class="form-control" readonly />
+                <input type="text" id="total41" name="total4" value="0" class="form-control" readonly />
+            </td>
+
+        </tr>
+
+        </table>
+
             <div class="form-group row">
                 <div class="col-md-12 text-center">
                     <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Save</button>
@@ -557,6 +618,19 @@
         });
 
         $('#totalInvoice').val(number_format(totalInvoice, 2));
+
+         $('#debet1').val(sisaBayar);
+         $('#debet21').val(number_format(sisaBayar, 2));
+
+         $('#kredit2').val(sisaBayar);
+         $('#kredit22').val(number_format(sisaBayar, 2));
+
+         $('#total').val(sisaBayar);
+         $('#total31').val(number_format(sisaBayar, 2));
+         $('#total2').val(sisaBayar);
+         $('#total41').val(number_format(sisaBayar, 2));
+
+
     }
 
     function number_format(number, decimals, dec_point, thousands_sep) {
