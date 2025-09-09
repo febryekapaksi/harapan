df9c027e2df29ceb057ac2a0a6901e5dec290741
SamSyamsudin <syams.69@gmail.com>
2025-09-09 17:20:43 +0700

    update jurnal incoming

--- application/modules/incoming_check/views/modal_incoming_check.php
+++ application/modules/incoming_check/views/modal_incoming_check.php
@@ -244,7 +244,7 @@
 		</div>
 	</div>
 
-	<div class="list_form">
+	<div class="list_form"> 
 		<?php
 		$sql = '
 			SELECT 
@@ -297,9 +297,78 @@
 		echo '<a href="' . base_url($file_incoming_material) . '" class="btn btn-sm btn-primary" style="margin-top: 2vh;" download><i class="fa fa-downloads"></i> Download File Incoming</a>';
 	}
 	?>
-
-
 </div>
+
+
+<h5>Informasi Jurnal</h5>
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr bgcolor='#9acfea'>
+            <th>
+                <center>Tanggal</center>
+            </th>
+            <th>
+                <center>Tipe</center>
+            </th>
+            <th>
+                <center>No. COA</center>
+            </th>
+            <th>
+                <center>Debit</center>
+            </th>
+            <th>
+                <center>Kredit</center>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr bgcolor='#DCDCDC'>
+            <td><input type="date" id="tgl_jurnal1" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
+            <td><input type="text" id="type1" name="type[]" value="JV" class="form-control" readonly /></td>
+            <td><input type="text" id="no_coa1" name="no_coa[]" value="1104-01-01" class="form-control" readonly /></td>
+            <td><input type="hidden" id="debet1" name="debet[]" value="<?= ?>" class="form-control" readonly />
+                <input type="text" id="debet21" name="debet2[]" value="<?= ?>" class="form-control" readonly />
+            </td>
+            <td><input type="hidden" id="kredit1" name="kredit[]" value="0" class="form-control" readonly />
+                <input type="text" id="kredit21" name="kredit2[]" value="0" class="form-control" readonly />
+            </td>
+
+        </tr>
+        <tr bgcolor='#DCDCDC'>
+            <td><input type="date" id="tgl_jurnal2" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
+            <td><input type="text" id="type2" name="type[]" value="JV" class="form-control" readonly /></td>
+            <td><input type="text" id="no_coa2" name="no_coa[]" value="1103-01-01" class="form-control" readonly /></td>
+            <td><input type="hidden" id="debet2" name="debet[]" value="0" class="form-control" readonly />
+                <input type="text" id="debet22" name="debet2[]" value="0" class="form-control" readonly />
+            </td>
+            <td><input type="hidden" id="kredit2" name="kredit[]" value="0" class="form-control" readonly />
+                <input type="text" id="kredit22" name="kredit2[]" value="0" class="form-control" readonly />
+            </td>
+
+        </tr>
+        <tr bgcolor='#DCDCDC'>
+            <td><input type="date" id="tgl_jurnal3" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
+            <td><input type="text" id="type3" name="type[]" value="JV" class="form-control" readonly /></td>
+            <td><input type="text" id="no_coa3" name="no_coa[]" value="2101-01-01" class="form-control" readonly /></td>
+            <td><input type="hidden" id="debet3" name="debet[]" value="0" class="form-control" readonly />
+                <input type="text" id="debet23" name="debet2[]" value="0" class="form-control" readonly />
+            </td>
+            <td><input type="hidden" id="kredit3" name="kredit[]" value="<?=  ?>" class="form-control" readonly />
+                <input type="text" id="kredit23" name="kredit2[]" value="<?=  ?>" class="form-control" readonly />
+            </td>
+
+        </tr>
+        <tr bgcolor='#DCDCDC'>
+            <td colspan="3" align="right"><b>TOTAL</b></td>
+            <td align="right"><input type="hidden" id="total" name="total" value="<?=  ?>" class="form-control" readonly />
+                <input type="text" id="total31" name="total3" value="<?=   ?>" class="form-control" readonly />
+            </td>
+            <td align="right"><input type="hidden" id="total2" name="total2" value="<?=  ?>" class="form-control" readonly />
+                <input type="text" id="total41" name="total4" value="<?=  ?>" class="form-control" readonly />
+            </td>
+
+        </tr>
+</table>
 <style>
 	.tanggal {
 		cursor: pointer;
