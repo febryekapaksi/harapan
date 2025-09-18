df92911eacbd49306f21df6eb691e2fbdda83b61
SamSyamsudin <syams.69@gmail.com>
2025-09-18 15:51:13 +0700

    update coa unbill

--- application/modules/incoming_check/views/modal_incoming_check.php
+++ application/modules/incoming_check/views/modal_incoming_check.php
@@ -356,7 +356,7 @@
 		<tr bgcolor='#DCDCDC'>
 			<td><input type="date" id="tgl_jurnal2" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
 			<td><input type="text" id="type2" name="type[]" value="JV" class="form-control" readonly /></td>
-			<td><input type="text" id="no_coa2" name="no_coa[]" value="1103-01-01" class="form-control" readonly /></td>
+			<td><input type="text" id="no_coa2" name="no_coa[]" value="2101-01-02" class="form-control" readonly /></td>
 			<td><input type="hidden" id="debet2" name="debet[]" value="0" class="form-control" readonly />
 				<input type="text" id="debet22" name="debet2[]" value="0" class="form-control" readonly />
 			</td>
@@ -368,7 +368,7 @@
 		<tr bgcolor='#DCDCDC'>
 			<td><input type="date" id="tgl_jurnal3" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
 			<td><input type="text" id="type3" name="type[]" value="JV" class="form-control" readonly /></td>
-			<td><input type="text" id="no_coa3" name="no_coa[]" value="2101-01-01" class="form-control" readonly /></td>
+			<td><input type="text" id="no_coa3" name="no_coa[]" value="1103-01-01" class="form-control" readonly /></td>
 			<td><input type="hidden" id="debet3" name="debet[]" value="0" class="form-control" readonly />
 				<input type="text" id="debet23" name="debet2[]" value="0" class="form-control" readonly />
 			</td>
