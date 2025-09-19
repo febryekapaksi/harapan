07bf6da3211875bb3789987e78b97430c9cdf0a7
SamSyamsudin <syams.69@gmail.com>
2025-09-19 10:37:17 +0700

    update nama coa setor bank

--- application/modules/setor_bank/views/form.php
+++ application/modules/setor_bank/views/form.php
@@ -119,6 +119,9 @@
                                         <th>
                                             <center>No. COA</center>
                                         </th>
+                                         <th>
+                                            <center>Nama. COA</center>
+                                        </th>
                                         <th>
                                             <center>Debit</center>
                                         </th>
@@ -132,7 +135,8 @@
                                         <td><input type="date" id="tgl_jurnal1" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
                                         <td><input type="text" id="type1" name="type[]" value="JV" class="form-control" readonly /></td>
                                         <td><input type="text" id="no_coa1" name="no_coa[]" class="form-control" readonly /></td>
-                                        <td><input type="hidden" id="debet1" name="debet[]" value="0" class="form-control" readonly />
+                                        <td><input type="text" id="nama_coa1" name="nama_coa[]" value="" class="form-control" readonly /></td>
+			                            <td><input type="hidden" id="debet1" name="debet[]" value="0" class="form-control" readonly />
                                             <input type="text" id="debet21" name="debet2[]" value="0" class="form-control" readonly />
                                         </td>
                                         <td><input type="hidden" id="kredit1" name="kredit[]" value="0" class="form-control" readonly />
@@ -144,7 +148,8 @@
                                         <td><input type="date" id="tgl_jurnal2" name="tgl_jurnal[]" value="<?= date('Y-m-d') ?>" class="form-control" readonly /></td>
                                         <td><input type="text" id="type2" name="type[]" value="JV" class="form-control" readonly /></td>
                                         <td><input type="text" id="no_coa2" name="no_coa[]" class="form-control" readonly /></td>
-                                        <td><input type="hidden" id="debet2" name="debet[]" value="0" class="form-control" readonly />
+                                        <td><input type="text" id="nama_coa2" name="nama_coa[]" value="Piutang Sales" class="form-control" readonly /></td>
+			                            <td><input type="hidden" id="debet2" name="debet[]" value="0" class="form-control" readonly />
                                             <input type="text" id="debet22" name="debet2[]" value="0" class="form-control" readonly />
                                         </td>
                                         <td><input type="hidden" id="kredit2" name="kredit[]" value="0" class="form-control" readonly />
@@ -235,9 +240,11 @@
             const noPerkiraan = $(this).val(); // value option
             const namaBank = $(this).find(':selected').data('nama'); // data-nama
 
-            $('input[name="no_coa[]"]').val(noPerkiraan);
+            //$('input[name="no_coa[]"]').val(noPerkiraan);
 
             $('#bank_name').val(namaBank);
+            $('#no_coa1').val(noPerkiraan);
+            $('#nama_coa1').val(namaBank);
         });
 
         // Tombol Pilih Penerimaan
