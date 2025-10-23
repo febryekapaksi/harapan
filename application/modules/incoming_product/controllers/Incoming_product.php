77aca60e94c2ecdaca52ee0a4898e3e786368535
febryekapaksi <febry.eka2012@gmail.com>
2025-10-23 15:14:32 +0700

    edit purchase order dan incoming

--- application/modules/incoming_product/controllers/Incoming_product.php
+++ application/modules/incoming_product/controllers/Incoming_product.php
@@ -85,7 +85,19 @@ class Incoming_product extends Admin_Controller
         $kode_supplier = $this->input->post('kode_supplier');
 
         $no_po = $this->db->query("
-            SELECT a.no_po, a.no_surat, a.status, 'PO' as ket_,b.nama AS nm_supplier FROM tr_purchase_order a LEFT JOIN new_supplier b ON b.kode_supplier = a.id_suplier WHERE a.status = '2' AND a.tipe IS NULL AND a.id_suplier = '" . $kode_supplier . "' AND (SELECT IF(SUM(aa.qty_oke + aa.qty_ng) IS NULL, 0, SUM(aa.qty_oke + aa.qty_ng)) FROM tr_checked_incoming_detail aa WHERE aa.no_ipp = a.no_po) < (SELECT SUM(ab.qty) FROM dt_trans_po ab WHERE ab.no_po = a.no_po) AND (SELECT COUNT(ac.id) FROM dt_trans_po ac JOIN new_inventory_4 ca ON ca.code_lv4 = ac.idmaterial WHERE ac.no_po = a.no_po AND ac.idmaterial <> '') > 0 AND a.close_po IS NULL ORDER BY a.no_po ASC
+            SELECT 
+                a.no_po, a.no_surat, a.status, 'PO' as ket_,b.nama AS nm_supplier 
+            FROM 
+                tr_purchase_order a 
+                LEFT JOIN new_supplier b ON b.kode_supplier = a.id_suplier 
+            WHERE a.status = '2' AND a.tipe IS NULL AND a.id_suplier = '" . $kode_supplier . "' AND (SELECT IF(SUM(aa.qty_oke + aa.qty_ng) IS NULL, 0, SUM(aa.qty_oke + aa.qty_ng)) 
+            FROM 
+                tr_checked_incoming_detail aa WHERE aa.no_ipp = a.no_po) < (SELECT SUM(ab.qty) FROM dt_trans_po ab WHERE ab.no_po = a.no_po) AND (SELECT COUNT(ac.id) 
+            FROM 
+                dt_trans_po ac 
+                JOIN new_inventory_4 ca ON ca.code_lv4 = ac.idmaterial 
+            WHERE 
+                ac.no_po = a.no_po AND ac.idmaterial <> '') > 0 AND a.close_po IS NULL ORDER BY a.no_po ASC
             ")
             ->result();
 
