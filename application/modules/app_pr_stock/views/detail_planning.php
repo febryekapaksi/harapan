773d8ec44b1d3ce927e4c0cf02bfe69aa657dc74
Alief Daffa <aliefdaffa572@gmail.com>
2025-11-03 15:56:48 +0700

    Perbaikan - Approval PR Stock

--- application/modules/app_pr_stock/views/detail_planning.php
+++ application/modules/app_pr_stock/views/detail_planning.php
@@ -36,28 +36,33 @@ $tgl_dibutuhkan = (!empty($header[0]['tgl_dibutuhkan'])) ? date('d F Y', strtoti
 						</tr>
 					</table>
 				</div>
+				<div class="col-md-6">
+					<label for="">Nilai Budget</label>
+					<input type="text" name="" id="" class="form-control form-control-sm text-right" value="<?= number_format($header[0]['nilai_budget']) ?>" readonly>
+				</div>
+				<div class="col-md-6">
+					<label for="">Nilai Pengajuan</label>
+					<input type="text" name="" id="" class="form-control form-control-sm text-right" value="<?= number_format($header[0]['nilai_pengajuan']) ?>" readonly>
+				</div>
 				<div class="col-md-12">
+					<br><br>
 					<table class='table table-striped table-bordered table-hover table-condensed' width='100%'>
 						<thead class='thead'>
 							<tr class='bg-blue'>
 								<th class='text-center th'>#</th>
-								<th class='text-center th'>Material Name</th>
-								<?php if ($pembeda == 'S') { ?>
-									<th class='text-center th'>Estimasi (Kg)</th>
-									<th class='text-center th'>Stock Free (Kg)</th>
-									<th class='text-center th'>Use Stock (Kg)</th>
-									<th class='text-center th'>Sisa Stock Free (Kg)</th>
-								<?php } ?>
-								<th class='text-center th'>Min Stock</th>
-								<th class='text-center th'>Max Stock</th>
-								<th class='text-center th'>Min Order</th>
-								<th class='text-center th'>Qty PR</th>
-								<th class='text-center th'>Qty Rev</th>
-								<th class='text-center th'>#</th>
+								<th class='text-center th'>Barang</th>
+								<th class='text-center th'>Kebutuhan 1 Bulan</th>
+								<th class='text-center th'>Max</th>
+								<th class='text-center th'>Stock</th>
+								<th class='text-center th'>Propose</th>
+								<th class='text-center th'>Price Reference</th>
+								<th class='text-center th'>Total Price</th>
 							</tr>
 						</thead>
 						<tbody>
 							<?php
+							$grand_total_price = 0;
+							$key = 0;
 							foreach ($detail as $key => $value) {
 								$key++;
 								$nm_material 	= $value['nm_material'];
@@ -66,42 +71,49 @@ $tgl_dibutuhkan = (!empty($header[0]['tgl_dibutuhkan'])) ? date('d F Y', strtoti
 								$sisa_free 		= $stock_free - $use_stock;
 								$propose 		= $value['propose_purchase'];
 
+								$get_material = $this->db->get_where('accessories', ['id' => $value['id_material']])->row();
+								$get_kebutuhan = $this->db->get_where('budget_rutin_detail', ['id_barang' => $value['id_material']])->row();
+								$get_stock = $this->db->get_where('warehouse_stock', ['id_material' => $value['id_material']])->row();
+
+								$kebutuhan = (!empty($get_kebutuhan)) ? $get_kebutuhan->kebutuhan_month : 0;
+								$stock = (!empty($get_stock)) ? $get_stock->qty_stock : 0;
+
+								$konversi = (!empty($get_material) && $get_material->konversi > 0) ? $get_material->konversi : 1;
+
 								echo "<tr>";
-								echo "<td class='text-center'>" . $key . "</td>";
-								echo "	<td class='text-left'>" . $nm_material . "
+								if ($value['status_app'] == 'N') {
+									echo "<td class='text-center'>" . $key . "</td>";
+								}
+								echo "<td class='text-left'>" . $nm_material . "
 										<input type='hidden' name='detail[" . $key . "][id]' value='" . $value['id'] . "'>
 										</td>";
-								if ($pembeda == 'S') {
-									echo "<td class='text-right qty_order'>" . number_format($value['qty_order'], 5) . "</td>";
-									echo "<td class='text-right stock_free'>" . number_format($stock_free, 5) . "</td>";
-									echo "<td class='text-right stock_free'>" . number_format($use_stock, 5) . "</td>";
-									echo "<td class='text-right sisa_free'>" . number_format($sisa_free, 5) . "</td>";
-								}
-								echo "<td class='text-right min_stok'>" . number_format($value['min_stok'], 2) . "</td>";
-								echo "<td class='text-right max_stok'>" . number_format($value['max_stok'], 2) . "</td>";
-								echo "<td class='text-right min_order'>" . number_format(0, 2) . "</td>";
-								echo "<td class='text-right'>" . number_format($propose, 2) . "</td>";
-								echo "<td class='text-center'>" . number_format($value['propose_rev'], 2) . "</td>";
-								if ($value['status_app'] == 'N') {
-									echo "<td class='text-center'><span class='badge bg-blue text-bold'>Waiting Process</span></td>";
-								}
-								if ($value['status_app'] == 'Y') {
-									echo "<td class='text-center'><span class='badge bg-green text-bold'>Approved</span></td>";
-								}
-								if ($value['status_app'] == 'D') {
-									echo "<td class='text-center'><span class='badge bg-red text-bold'>Rejected</span></td>";
-								}
+								echo "<td class='text-right min_stok'>" . number_format($kebutuhan) . "</td>";
+								echo "<td class='text-right max_stok'>" . number_format($kebutuhan * 1.5) . "</td>";
+								echo "<td class='text-right min_order'>" . number_format($stock) . "</td>";
+								echo "<td class='text-right'>" . number_format($propose * $konversi) . "</td>";
+								echo "<td class='text-right'>Rp. " . number_format($get_material->price_ref_high) . "</td>";
+								echo "<td class='text-right'>Rp. " . number_format(($propose * $konversi) * $get_material->price_ref_high) . "</td>";
+
 								echo "</tr>";
+
+								$grand_total_price += (($propose * $konversi) * $get_material->price_ref_high);
 							}
 							?>
 						</tbody>
+						<tfoot>
+							<tr class="bg-blue">
+								<th colspan="7" class="text-center">Total Price Pengajuan</th>
+								<th class="text-right">Rp. <?= number_format($grand_total_price) ?></th>
+							</tr>
+						</tfoot>
 					</table>
 				</div>
 			</div>
 
 			<div class="form-group row">
-				<div class="text-center">
-					<button type="button" class="btn btn-default" name="back" id="back"><i class="fa fa-reply"></i> Kembali</button>
+				<div class="col-md-12">
+					<!-- <button type="button" class="btn btn-primary" name="save" id="save">Process</button> -->
+					<button type="button" class="btn btn-danger" style='margin-left:5px;' name="back" id="back">Back</button>
 				</div>
 			</div>
 		</form>
@@ -141,7 +153,7 @@ $tgl_dibutuhkan = (!empty($header[0]['tgl_dibutuhkan'])) ? date('d F Y', strtoti
 
 			//back
 			$(document).on('click', '#back', function() {
-				window.location.href = base_url + active_controller
+				window.location.href = base_url + active_controller + '/approval_management'
 			});
 		});
 	</script>
