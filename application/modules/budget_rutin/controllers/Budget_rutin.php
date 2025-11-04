90dea051606bbad163954e1a79b047191b7401f1
Alief Daffa <aliefdaffa572@gmail.com>
2025-11-04 16:56:47 +0700

    Perbaikan - Budget Stock

--- application/modules/budget_rutin/controllers/Budget_rutin.php
+++ application/modules/budget_rutin/controllers/Budget_rutin.php
@@ -208,7 +208,7 @@ class Budget_rutin extends Admin_Controller
 
         $get_price_ref = $this->db->get_where('accessories', ['id' => $id_barang])->row();
 
-        $price_ref = (!empty($get_price_ref)) ? $get_price_ref->price_ref_high : 0;
+        $price_ref = (!empty($get_price_ref)) ? $get_price_ref->price_ref : 0;
 
         echo json_encode([
             'nilai_price_ref' => $price_ref
