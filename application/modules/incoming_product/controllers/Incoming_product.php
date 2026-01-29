0771aa7904bc19130aabf96e24ee9b5ce1534204
febryekapaksi <febry.eka2012@gmail.com>
2026-01-29 09:52:28 +0700

    ubah query untuk incoming

--- application/modules/incoming_product/controllers/Incoming_product.php
+++ application/modules/incoming_product/controllers/Incoming_product.php
@@ -32,18 +32,38 @@ class Incoming_product extends Admin_Controller
     public function index()
     {
         $this->auth->restrict($this->viewPermission);
+
         $session = $this->session->userdata('app_session');
         $this->template->page_icon('fa fa-users');
+
         $data_Group = $this->db->query('SELECT * FROM `groups`')->result_array();
-        $pusat      = $this->db->query("SELECT * FROM `warehouse` WHERE `desc`='pusat' ORDER BY `urut` ASC")->result_array();
-        $no_po        = $this->db->query("
-										SELECT a.no_po, a.no_surat, a.status, 'PO' as ket_,b.nama AS nm_supplier FROM tr_purchase_order a LEFT JOIN new_supplier b ON b.kode_supplier = a.id_suplier WHERE a.status = '2' AND a.tipe IS NULL AND (SELECT IF(SUM(aa.qty_oke + aa.qty_ng) IS NULL, 0, SUM(aa.qty_oke + aa.qty_ng)) FROM tr_checked_incoming_detail aa WHERE aa.no_ipp = a.no_po) < (SELECT SUM(ab.qty) FROM dt_trans_po ab WHERE ab.no_po = a.no_po) AND (SELECT COUNT(ac.id) FROM dt_trans_po ac JOIN new_inventory_4 ca ON ca.code_lv4 = ac.idmaterial WHERE ac.no_po = a.no_po AND ac.idmaterial <> '') > 0 ORDER BY a.no_po ASC
-										")->result_array();
-        $list_po    = $this->db->group_by('no_ipp')->get_where('warehouse_adjustment', array('category' => 'incoming product'))->result_array();
-        $data_gudang = $this->db->group_by('id_gudang_ke')->get_where('warehouse_adjustment', array('category' => 'incoming product'))->result_array();
+
+        $pusat = $this->db->query("SELECT * FROM `warehouse` WHERE `desc`='pusat' ORDER BY `urut` ASC")->result_array();
+
+        $no_po = $this->db->query("
+        SELECT a.no_po, a.no_surat, a.status, 'PO' as ket_, b.nama AS nm_supplier 
+        FROM tr_purchase_order a 
+        LEFT JOIN new_supplier b ON b.kode_supplier = a.id_suplier 
+        WHERE a.status = '2' 
+        AND a.tipe IS NULL 
+        AND (SELECT IF(SUM(aa.qty_oke + aa.qty_ng) IS NULL, 0, SUM(aa.qty_oke + aa.qty_ng)) 
+            FROM tr_checked_incoming_detail aa WHERE aa.no_ipp = a.no_po) < 
+            (SELECT SUM(ab.qty) FROM dt_trans_po ab WHERE ab.no_po = a.no_po) 
+        AND (SELECT COUNT(ac.id) 
+            FROM dt_trans_po ac 
+            JOIN new_inventory_4 ca ON ca.code_lv4 = ac.idmaterial 
+            WHERE ac.no_po = a.no_po AND ac.idmaterial <> '') > 0 
+        GROUP BY a.no_po, a.no_surat, a.status, b.nama 
+        ORDER BY a.no_po ASC
+    ")->result_array();
+
+        $list_po = $this->db->group_by('no_ipp')->get_where('warehouse_adjustment', array('category' => 'incoming material'))->result_array();
+
+        $data_gudang = $this->db->group_by('id_gudang_ke')->get_where('warehouse_adjustment', array('category' => 'incoming material'))->result_array();
 
         $list_supplier = $this->db->select('kode_supplier, nama')->get_where('new_supplier', ['deleted_by' => null])->result();
 
+        // Menyiapkan data untuk dikirim ke view
         $data = array(
             'action'            => 'index',
             'row_group'         => $data_Group,
@@ -53,10 +73,13 @@ class Incoming_product extends Admin_Controller
             'no_po'             => $no_po,
             'list_supplier'     => $list_supplier
         );
-        // history('View Incoming Material');
+
+        history('View Incoming Material');
+
+        // Mengirim data ke template dan merender halaman
         $this->template->set($data);
         $this->template->page_icon('fa fa-sign-in');
-        $this->template->title('Incoming Product');
+        $this->template->title('Incoming Material');
         $this->template->render('index');
     }
 
