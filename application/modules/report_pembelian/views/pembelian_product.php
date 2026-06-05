35ed36fcd96544a47d8388b5b90e9de3f3614486
SamSyamsudin <syams.69@gmail.com>
2026-06-05 09:58:38 +0700

    ubah report per barang karena beda yang di faktur dengan per barang

--- application/modules/report_pembelian/views/pembelian_product.php
+++ application/modules/report_pembelian/views/pembelian_product.php
@@ -28,6 +28,7 @@
                 <thead>
                     <tr class="bg-blue">
                         <th width="50" class="text-center">No</th>
+                        <th width="150" class="text-center">No Invoice</th>
                         <th class="text-center">Nama Barang</th>
                         <th width="150" class="text-center">Kts (Unit#1)</th>
                         <th width="200" class="text-center">Total Pembelian</th>
@@ -80,7 +81,7 @@
             "searching": true,
             "responsive": true,
             "aaSorting": [
-                [3, "desc"]
+                [4, "desc"]
             ],
             "columnDefs": [{
                     "targets": [0],
@@ -89,10 +90,14 @@
                 },
                 {
                     "targets": [1],
+                    "className": "text-center"
+                },
+                {
+                    "targets": [2],
                     "className": "text-left"
                 },
                 {
-                    "targets": [2, 3],
+                    "targets": [3, 4],
                     "className": "text-right"
                 }
             ],
@@ -112,7 +117,7 @@
                 cache: false,
                 error: function() {
                     $(".my-grid-error").html("");
-                    $("#tableItem").append('<tbody class="my-grid-error"><tr><th colspan="4">No data found in the server</th></tr></tbody>');
+                    $("#tableItem").append('<tbody class="my-grid-error"><tr><th colspan="5">No data found in the server</th></tr></tbody>');
                     $("#tableItem_processing").css("display", "none");
                 }
             }
