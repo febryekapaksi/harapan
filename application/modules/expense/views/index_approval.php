4929f205ae61d70e3c4eedeeaa1385a991be293a
alief572 <146901652+alief572@users.noreply.github.com>
2026-08-26 11:40:58 +0700

    Fix: Expense dan Pengembalian Expense

--- application/modules/expense/views/index_approval.php
+++ application/modules/expense/views/index_approval.php
@@ -4,22 +4,46 @@ $ENABLE_MANAGE  = has_permission('Expense_Approval.Manage');
 $ENABLE_VIEW    = has_permission('Expense_Approval.View');
 $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 ?>
+<link rel="stylesheet" href="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.css') ?>">
+
+<style>
+	#mytabledata_approval thead th {
+		background-color: #3c8dbc;
+		color: #ffffff;
+		text-align: center;
+		vertical-align: middle;
+		border: 1px solid #367fa9;
+	}
+	#mytabledata_approval tbody td {
+		vertical-align: middle;
+	}
+	.badge {
+		font-size: 11px;
+		padding: 4px 8px;
+		font-weight: 600;
+	}
+</style>
+
 <div id="alert_edit" class="alert alert-success alert-dismissable" style="padding: 15px; display: none;"></div>
-<div class="box">
+
+<div class="box box-primary">
+	<div class="box-header with-border">
+		<h3 class="box-title"><i class="fa fa-check-square-o"></i> Daftar Pengajuan Expense Menunggu Approval</h3>
+	</div>
 	<!-- /.box-header -->
 	<div class="box-body">
-		<div class="table-responsive col-md-12">
-			<table id="mytabledata" class="table table-bordered table-striped">
+		<div class="table-responsive">
+			<table id="mytabledata_approval" class="table table-bordered table-striped table-hover" width="100%">
 				<thead>
 					<tr>
-						<th width="5">#</th>
-						<th>No Dokumen</th>
-						<th>Tanggal</th>
-						<th>Nama</th>
-						<th>Keterangan</th>
-						<th>Nominal</th>
-						<th>Status</th>
-						<th width="120">Action</th>
+						<th width="4%">#</th>
+						<th width="15%">No Dokumen</th>
+						<th width="10%">Tanggal</th>
+						<th width="15%">Nama</th>
+						<th width="20%">Keterangan</th>
+						<th width="14%">Nominal</th>
+						<th width="10%">Status</th>
+						<th width="12%">Action</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -27,22 +51,29 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 					if (!empty($data)) {
 						$numb = 0;
 						foreach ($data as $record) {
-							$numb++; ?>
+							$numb++; 
+							$tgl_doc = (!empty($record->tgl_doc) && $record->tgl_doc != '0000-00-00') ? date('d M Y', strtotime($record->tgl_doc)) : '-';
+							?>
 							<tr>
-								<td><?= $numb; ?></td>
-								<td><?= $record->no_doc ?></td>
-								<td><?= $record->tgl_doc ?></td>
-								<td><?= $record->nmuser ?></td>
+								<td class="text-center"><?= $numb; ?></td>
+								<td class="text-center"><b><?= $record->no_doc ?></b></td>
+								<td class="text-center"><?= $tgl_doc ?></td>
+								<td><?= $record->nmuser ?: $record->nama ?></td>
 								<td><?= $record->informasi ?></td>
-								<td class="text-right"><?= number_format($record->nominal) ?></td>
-								<td><?= $status[$record->status] ?></td>
-								<td>
+								<td class="text-right" style="font-weight: 600;"><?= number_format($record->nominal, 0, ',', '.') ?></td>
+								<td class="text-center">
+									<?php
+									$st_label = isset($status[$record->status]) ? $status[$record->status] : 'Menunggu Approval';
+									echo '<span class="badge bg-yellow">' . $st_label . '</span>';
+									?>
+								</td>
+								<td class="text-center" style="white-space: nowrap;">
 									<?php if ($ENABLE_VIEW) : ?>
-										<a class="btn btn-warning btn-sm view" href="javascript:void(0)" title="View" onclick="data_view('<?= $record->id ?>')"><i class="fa fa-eye"></i></a>
-										<?php endif;
+										<a class="btn btn-info btn-xs" href="javascript:void(0)" title="Lihat Detail" onclick="data_view('<?= $record->id ?>')"><i class="fa fa-eye"></i> Detail</a>
+									<?php endif;
 									if ($ENABLE_MANAGE) :
 										if ($record->status == 0) { ?>
-											<a class="btn btn-success btn-sm approve" href="javascript:void(0)" title="Approve" onclick="data_approve('<?= $record->id ?>')"><i class="fa fa-check-square-o"></i></a>
+											<a class="btn btn-success btn-xs" href="javascript:void(0)" title="Proses Approval" onclick="data_approve('<?= $record->id ?>')"><i class="fa fa-check-square-o"></i> Approve</a>
 									<?php }
 									endif;
 									?>
@@ -58,13 +89,16 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 	<!-- /.box-body -->
 </div>
 <div id="form-data"></div>
+
 <script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/basic.js') ?>"></script>
+
 <script type="text/javascript">
 	var url_edit = siteurl + 'expense/edit/';
 	var url_view = siteurl + 'expense/view/';
 	var url_approval = siteurl + 'expense/approval/';
-	//Edit
+
 	function data_approve(id) {
 		if (id != "") {
 			$(".box").hide();
@@ -72,5 +106,11 @@ $ENABLE_DELETE  = has_permission('Expense_Approval.Delete');
 			$("#form-data").load(url_approval + id);
 		}
 	}
+
+	$(document).ready(function() {
+		$("#mytabledata_approval").DataTable({
+			"responsive": true,
+			"order": [[1, "desc"]]
+		});
+	});
 </script>
-<script src="<?= base_url('assets/js/basic.js') ?>"></script>
--- application/modules/expense/views/transport_req_fin_list.php
+++ application/modules/expense/views/transport_req_fin_list.php
@@ -4,24 +4,47 @@ $ENABLE_MANAGE  = has_permission('Pengajuan_Transportasi_Approval.Manage');
 $ENABLE_VIEW    = has_permission('Pengajuan_Transportasi_Approval.View');
 $ENABLE_DELETE  = has_permission('Pengajuan_Transportasi_Approval.Delete');
 ?>
+<link rel="stylesheet" href="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.css') ?>">
+
+<style>
+	#mytabledata_transport_req_fin thead th {
+		background-color: #3c8dbc;
+		color: #ffffff;
+		text-align: center;
+		vertical-align: middle;
+		border: 1px solid #367fa9;
+	}
+	#mytabledata_transport_req_fin tbody td {
+		vertical-align: middle;
+	}
+	.badge {
+		font-size: 11px;
+		padding: 4px 8px;
+		font-weight: 600;
+	}
+</style>
+
 <div id="alert_edit" class="alert alert-success alert-dismissable" style="padding: 15px; display: none;"></div>
-<link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
-<div class="box">
+
+<div class="box box-primary">
+	<div class="box-header with-border">
+		<h3 class="box-title"><i class="fa fa-check-square-o"></i> Approval Request Transportasi (Finance)</h3>
+	</div>
+	<!-- /.box-header -->
 	<div class="box-body">
 		<div class="table-responsive">
-			<table id="mytabledata" class="table table-bordered table-striped">
+			<table id="mytabledata_transport_req_fin" class="table table-bordered table-striped table-hover" width="100%">
 				<thead>
 					<tr>
-						<th width="5">#</th>
-						<th>No</th>
-						<th>Tanggal</th>
-						<th>Nama</th>
-						<th>Status</th>
-						<th width="120">Action</th>
+						<th width="4%">#</th>
+						<th width="20%">No Dokumen</th>
+						<th width="15%">Tanggal</th>
+						<th width="25%">Nama</th>
+						<th width="15%">Status</th>
+						<th width="15%">Action</th>
 					</tr>
 				</thead>
 				<tbody>
-
 				</tbody>
 			</table>
 		</div>
@@ -29,104 +52,93 @@ $ENABLE_DELETE  = has_permission('Pengajuan_Transportasi_Approval.Delete');
 	<!-- /.box-body -->
 </div>
 <div id="form-data"></div>
+
 <!-- DataTables -->
-<script src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
+<script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/basic.js') ?>"></script>
+
 <!-- page script -->
 <script type="text/javascript">
 	var url_view = siteurl + 'expense/transport_req_view/';
 	var url_approve = siteurl + 'expense/transport_req_approve/';
 
-	datatables();
+	$(document).ready(function() {
+		datatables();
+	});
 
 	function data_approve(id) {
 		swal({
-				title: "Anda Yakin?",
-				text: "Data Akan Disetujui!",
-				type: "info",
-				showCancelButton: true,
-				confirmButtonText: "Ya, setuju!",
-				cancelButtonText: "Tidak!",
-				closeOnConfirm: false,
-				closeOnCancel: true
-			},
-			function(isConfirm) {
-				if (isConfirm) {
-					$.ajax({
-						url: url_approve + id + '/1',
-						dataType: "json",
-						type: 'POST',
-						success: function(msg) {
-							if (msg['save'] == '1') {
-								swal({
-									title: "Sukses!",
-									text: "Data Berhasil Di Setujui",
-									type: "success",
-									timer: 1500,
-									showConfirmButton: false
-								});
-								window.location.reload();
-							} else {
-								swal({
-									title: "Gagal!",
-									text: "Data Gagal Di Setujui",
-									type: "error",
-									timer: 1500,
-									showConfirmButton: false
-								});
-							};
-							console.log(msg);
-						},
-						error: function(msg) {
+			title: "Anda Yakin?",
+			text: "Pengajuan transportasi ini akan disetujui!",
+			type: "info",
+			showCancelButton: true,
+			confirmButtonText: "Ya, Setujui!",
+			cancelButtonText: "Batal",
+			closeOnConfirm: false,
+			closeOnCancel: true
+		},
+		function(isConfirm) {
+			if (isConfirm) {
+				$.ajax({
+					url: url_approve + id + '/1',
+					dataType: "json",
+					type: 'POST',
+					success: function(msg) {
+						if (msg['save'] == '1') {
+							swal({
+								title: "Sukses!",
+								text: "Data berhasil disetujui",
+								type: "success",
+								timer: 1500,
+								showConfirmButton: false
+							});
+							window.location.reload();
+						} else {
 							swal({
 								title: "Gagal!",
-								text: "Ajax Data Gagal Di Proses",
+								text: "Data gagal disetujui",
 								type: "error",
 								timer: 1500,
 								showConfirmButton: false
 							});
-							console.log(msg);
 						}
-					});
-				}
-			});
+					},
+					error: function(msg) {
+						swal({
+							title: "Gagal!",
+							text: "Terjadi kesalahan AJAX, silakan coba lagi.",
+							type: "error",
+							timer: 1500,
+							showConfirmButton: false
+						});
+					}
+				});
+			}
+		});
 	}
 
 	function datatables() {
-		var datatables = $('#mytabledata').dataTable({
+		var datatables = $('#mytabledata_transport_req_fin').dataTable({
 			serverSide: true,
 			processing: true,
-			stateSave: true,
-			paging: true,
+			responsive: true,
 			destroy: true,
+			order: [[1, 'desc']],
 			ajax: {
 				type: 'post',
 				url: siteurl + active_controller + 'get_data_transport_req_fin_list',
 				cache: false,
-				dataType: 'json',
-				data: function(d) {
-
-				}
+				dataType: 'json'
 			},
-			columns: [{
-					data: 'no'
-				},
-				{
-					data: 'no_transport'
-				},
-				{
-					data: 'tanggal'
-				},
-				{
-					data: 'nama'
-				},
-				{
-					data: 'status'
-				},
-				{
-					data: 'action'
-				}
+			columns: [
+				{ data: 'no', className: 'text-center' },
+				{ data: 'no_transport', className: 'text-center' },
+				{ data: 'tanggal', className: 'text-center' },
+				{ data: 'nama' },
+				{ data: 'status', className: 'text-center' },
+				{ data: 'action', className: 'text-center' }
 			]
-		})
+		});
 	}
 </script>
-<script src="<?= base_url('assets/js/basic.js') ?>"></script>
